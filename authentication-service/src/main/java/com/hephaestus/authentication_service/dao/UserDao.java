a28671cfde39fa13385718b1e91248a450b8009b
viveksvijay <vivekkollam@outlook.in>
2024-06-30 22:29:40 +0530

    authentication service removed

--- authentication-service/src/main/java/com/hephaestus/authentication_service/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.hephaestus.authentication_service.dao;
-
-import com.hephaestus.authentication_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUserName(String userName);
-}
--- authentication-service/src/main/java/com/hephaestus/authentication_service/model/AuthenticationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.hephaestus.authentication_service.model;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationRequest {
-    private String username;
-    private String password;
-}
